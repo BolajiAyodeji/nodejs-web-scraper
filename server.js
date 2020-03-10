0299288ca775e8a0d7801e3bd8c326a664c57a84
bolajiayodeji <sirbeejay1@gmail.com>
2020-03-10 07:37:14 +0100

    format :zap:

--- server.js
+++ server.js
@@ -1,24 +1,26 @@
-const axios = require('axios');
-const cheerio = require('cheerio');
+const axios = require("axios");
+const cheerio = require("cheerio");
 
-    const url = 'https://news.ycombinator.com';
+const url = "https://news.ycombinator.com";
 
-    axios(url)
-      .then(response => {
-        const html = response.data;
+axios(url)
+  .then(response => {
+    const html = response.data;
 
-        let getData = html => {
-          data = [];
-          const $ = cheerio.load(html);
-          $('table.itemlist tr td:nth-child(3)').each((i, elem) => {
-            data.push({
-              title : $(elem).text(),
-              link : $(elem).find('a.storylink').attr('href')
-            });
-          });
-          console.log(data);
-        }
-        
-        getData(html)
-      })
-      .catch(console.error);
+    let getData = html => {
+      data = [];
+      const $ = cheerio.load(html);
+      $("table.itemlist tr td:nth-child(3)").each((i, elem) => {
+        data.push({
+          title: $(elem).text(),
+          link: $(elem)
+            .find("a.storylink")
+            .attr("href")
+        });
+      });
+      console.log(data);
+    };
+
+    getData(html);
+  })
+  .catch(console.error);
