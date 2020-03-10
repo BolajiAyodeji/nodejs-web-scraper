2683d6daad0e278bd9a1f18996dfaffe8e04c084
bolajiayodeji <sirbeejay1@gmail.com>
2020-03-10 02:36:54 +0100

    :unicorn:

---
+++ static/hn-scraper.js
@@ -0,0 +1,26 @@
+const axios = require("axios");
+const cheerio = require("cheerio");
+
+const url = "https://news.ycombinator.com";
+
+axios(url)
+  .then(response => {
+    const html = response.data;
+
+    let getData = html => {
+      data = [];
+      const $ = cheerio.load(html);
+      $("table.itemlist tr td:nth-child(3)").each((i, elem) => {
+        data.push({
+          title: $(elem).text(),
+          link: $(elem)
+            .find("a.storylink")
+            .attr("href")
+        });
+      });
+      console.log(data);
+    };
+
+    getData(html);
+  })
+  .catch(console.error);
